dcd1ef46598a1048c84b95688e8bbbe5efc7364e
Michelle Wong <michellekwong2@gmail.com>
2021-03-18 17:13:23 -0400

    Created auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignoutComponent } from './components/signout/signout.component';
+import { AuthGuard } from 'src/app/auth-guard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: 'home', pathMatch: 'full'},
+  {path: "home", component: HomeComponent},
+  {path: "contact", component: ContactusComponent},
+  {path: "login", component: LoginComponent},
+  {path: "signout", component: SignoutComponent},
+  //using AuthGuard
+  {path:"admin", canActivate: [AuthGuard], component: AdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
