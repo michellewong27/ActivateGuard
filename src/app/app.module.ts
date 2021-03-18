6f3e24f80fe6fcfbe41a315c5372a55d463e347d
Michelle Wong <michellekwong2@gmail.com>
2021-03-18 17:05:18 -0400

    Folders & components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignoutComponent } from './components/signout/signout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ContactusComponent,
+    HomeComponent,
+    LoginComponent,
+    SignoutComponent
   ],
   imports: [
     BrowserModule,
